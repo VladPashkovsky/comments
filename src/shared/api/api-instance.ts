41c5730939911b593034c2e4e027fca1a93a9e8e
VladPashkovsky <v.v.pashkovsky@gmail.com>
2024-11-19 16:29:49 +0100

    shared/api_added__components/loginstore_added

---
+++ src/shared/api/api-instance.ts
@@ -0,0 +1,42 @@
+import loginStore from '../../components/Login/loginStore.ts'
+
+const BASE_URL = 'http://localhost:8000/api'
+
+class ApiError extends Error {
+  constructor(public response: Response) {
+    super('ApiError' + response.status)
+  }
+}
+
+export const jsonApiInstance = async <T>(
+  url: string,
+  init?: RequestInit & { json?: unknown },
+) => {
+  let headers = init?.headers ?? {}
+
+  const token = loginStore.getState().user?.accessToken
+  if (token) {
+    headers = {
+      Authorization: `Bearer ${token}`,
+      ...headers,
+    }
+  }
+
+  if (init?.json) {
+    headers = {
+      'Content-Type': 'application/json',
+      ...headers,
+    }
+    init.body = JSON.stringify(init.json)
+  }
+
+  const result = await fetch(`${BASE_URL}${url}`, {
+    ...init, headers,
+  })
+
+  if (!result.ok) {
+    throw new ApiError(result)
+  }
+  const data = (await result.json()) as Promise<T>
+  return data
+}
