21bffe4e083ff3d30ef285ddb189f09123449ed2
VladPashkovsky <v.v.pashkovsky@gmail.com>
2025-02-03 18:17:46 +0100

    eslint.config.js_changed

--- eslint.config.js
+++ eslint.config.js
@@ -3,28 +3,19 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import jestPlugin from 'eslint-plugin-jest'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...jestPlugin.configs.recommended,
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        ...globals.jest,
-      },
+      globals: globals.browser,
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      'jest': jestPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -32,7 +23,6 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      'jest/no-focused-test': 'off',
     },
   },
 )
