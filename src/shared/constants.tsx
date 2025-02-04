d71700b16bf6defc8b44a392823e9ca36593cd21
VladPashkovsky <v.v.pashkovsky@gmail.com>
2025-02-04 18:59:57 +0100

    constants_text_changed

--- src/shared/constants.tsx
+++ src/shared/constants.tsx
@@ -8,9 +8,9 @@ export const aboutRus: React.ReactNode = (
     <ul style={{marginLeft: '25px'}}>
       <li>JWT авторизация/регистрация нового пользователя.</li>
       <li>Добавление/изменение аватара пользователя с сохранением в объектном хранилище S3.</li>
-      <li>Добавление пользователем комментариев либо создание комментариев автоматически.</li>
+      <li>Добавление/удаление пользователем комментариев либо создание комментариев автоматически.</li>
       <li>Бесконечный скролл с постепенной подгрузкой комментариев, созданных пользователем из базы данных PostgreSQL.</li>
-      <li>Удаление комментариев пользователя реализовано через Optimistic Updates.</li>
+      <li>Настроен CI/CD через GitHub Actions, с дальнейшим развертыанием в Docker на Linux сервере.</li>
     </ul>
     <br/>
     <p style={{ textAlign: 'center' }}>Вход - name: admin, password: 123456789 либо зарегистрируйтесь в качестве нового пользователя.</p>
@@ -26,9 +26,9 @@ export const aboutEng: React.ReactNode = (
     <ul style={{marginLeft: '25px'}}>
       <li>JWT authorization/registration of a new user.</li>
       <li>Adding/changing a user avatar with saving in S3 cloud object storage.</li>
-      <li>Adding comments by the user or creating comments automatically.</li>
+      <li>Adding/deleting comments by the user or creating comments automatically.</li>
       <li>Infinite scroll with gradual loading of comments created by the user from the PostgreSQL database.</li>
-      <li>Deleting user comments is implemented using Optimistic Updates.</li>
+      <li>Created a CI/CD Pipeline with Docker and GitHub Actions.</li>
     </ul>
     <br/>
     <p style={{ textAlign: 'center' }}>Login - name: admin, password: 123456789 or register as a new user.</p>
